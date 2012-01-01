8b503abfe06d7ad87b38c264219f0a6b6d0df6c4
Unai Aguilera <gkalgan@gmail.com>
2012-01-01 17:06:16 +0000

    * Previously commited changed reverted.

--- src/peer/message/ACKMessage.java
+++ src/peer/message/ACKMessage.java
@@ -3,7 +3,7 @@ package peer.message;
 import java.io.IOException;
 import java.io.ObjectInput;
 import java.io.ObjectOutput;
-import java.util.Collections;
+import java.util.ArrayList;
 
 import peer.peerid.PeerID;
 import serialization.binary.UnserializationUtils;
@@ -22,7 +22,7 @@ public class ACKMessage extends BroadcastMessage {
 	}
 
 	public ACKMessage(final PeerID sender, final MessageID respondingTo) {
-		super(sender, Collections.singletonList(respondingTo.getPeer()));
+		super(sender, new ArrayList<PeerID>());
 		this.respondingTo = respondingTo;
 	}
 
