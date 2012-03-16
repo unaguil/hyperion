92aceacc7504a6452d150ab26d346cd456cf2612
Unai Aguilera <gkalgan@gmail.com>
2012-03-16 18:36:10 +0000

    * Due to message order connections were removed previous disconnect message notification were received.

--- src/graphcreation/collisionbased/connectionManager/ConnectionsManager.java
+++ src/graphcreation/collisionbased/connectionManager/ConnectionsManager.java
@@ -12,6 +12,7 @@ import java.util.Map.Entry;
 import java.util.Set;
 
 import multicast.search.message.SearchResponseMessage;
+import peer.conditionregister.ConditionRegister;
 import peer.peerid.PeerID;
 import peer.peerid.PeerIDSet;
 import taxonomy.Taxonomy;
@@ -21,6 +22,10 @@ public class ConnectionsManager {
 
 	// collisions detected by the current node
 	private final Set<Connection> detectedConnections = new HashSet<Connection>();
+	
+	private static final long CLEAN_INVALID_TIME = 60000;
+	
+	private final ConditionRegister<Connection> invalidConnections = new ConditionRegister<Connection>(CLEAN_INVALID_TIME);
 	
 	private final GraphType graphType;
 
@@ -63,9 +68,18 @@ public class ConnectionsManager {
 		return updatedConnections;
 	}
 
+	/**
+	 * Removes those responses identified by the passed route identifiers
+	 * 
+	 * @param lostRoutes
+	 *            the routes which have been lost
+	 * @return the notifications for colliding peers
+	 */
 	public Map<PeerIDSet, Set<Service>> removeResponses(final Set<PeerID> lostDestinations) {
 		final Map<PeerIDSet, Set<Service>> notifications = new HashMap<PeerIDSet, Set<Service>>();
-		for (Iterator<Connection> it = detectedConnections.iterator(); it.hasNext(); ) {
+		final Set<Connection> removedConnections = new HashSet<Connection>(); 
+				
+		for (Iterator<Connection> it = getAllConnections().iterator(); it.hasNext(); ) {
 			Connection connection = it.next();
 			final Map<PeerIDSet, Set<Service>> partialNotifications = connection.removeResponses(lostDestinations);
 			for (final Entry<PeerIDSet, Set<Service>> entry : partialNotifications.entrySet()) {
@@ -76,9 +90,19 @@ public class ConnectionsManager {
 			}
 			
 			if (!connection.isConnected())
-				it.remove();
+				removedConnections.add(connection);
 		}
+		
+		removeConnections(removedConnections);
+		
 		return notifications;
+	}
+	
+	private void removeConnections(final Set<Connection> removedConnections) {
+		for (final Connection removedConnection : removedConnections) {
+			invalidConnections.remove(removedConnection);
+			detectedConnections.remove(removedConnection);
+		}
 	}
 
 	/**
@@ -93,7 +117,7 @@ public class ConnectionsManager {
 	public Map<PeerIDSet, Set<Service>> removeServices(final Set<Service> removedServices, final PeerID source) {
 		final Map<PeerIDSet, Set<Service>> notifications = new HashMap<PeerIDSet, Set<Service>>();
 		
-		for (final Connection connection : detectedConnections) {
+		for (final Connection connection : getAllConnections()) {
 			final Map<PeerIDSet, Set<Service>> partialNotifications = connection.removeServices(removedServices, source);
 			for (final Entry<PeerIDSet, Set<Service>> entry : partialNotifications.entrySet()) {
 				final PeerIDSet peers = entry.getKey();
@@ -103,6 +127,13 @@ public class ConnectionsManager {
 			}
 		}
 		return notifications;
+	}
+
+	private Set<Connection> getAllConnections() {
+		//include valid & invalid connections
+		final Set<Connection> checkedConnections = new HashSet<Connection>(detectedConnections);
+		checkedConnections.addAll(invalidConnections.getEntries());
+		return checkedConnections;
 	}
 
 	/**
@@ -120,6 +151,7 @@ public class ConnectionsManager {
 			if (removedParameters.contains(collision.getInput()) || removedParameters.contains(collision.getOutput())) {
 				connections.add(connection);
 				it.remove();
+				invalidConnections.addEntry(connection);
 			}
 		}
 
